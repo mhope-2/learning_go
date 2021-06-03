e044bb741cea456149357a1441e122b0443695ce
mhope <mhope@npontu.com>
2021-06-03 00:41:32 +0000

    refactor(hash): added hash file

--- src/hash_non_cryptographic.go
+++ src/hash_non_cryptographic.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"hash/crc32"
+	"io"
+	"os"
 )
 
 func hasherInit(){
@@ -15,6 +17,36 @@ func hasherInit(){
 	fmt.Println(v)
 }
 
-func main()  {
+// check file similarity
+func getHash(filename string) (uint32, error){
+	// open the file
+	f, err := os.Open(filename)
+	if err != nil {
+		return 0, err
+	}
+	// close opened file
+	defer f.Close()
 
+	// create a hasher
+	h := crc32.NewIEEE()
+	// copy the file into the hasher
+	// - copy takes (dst, src) and returns (bytesWritten, error)
+	_, err = io.Copy(h, f)
+	// we don't care about how many bytes were written, but we do want to handle the error
+	if err != nil {
+		return 0, err
+	}
+	return h.Sum32(), nil
+}
+
+func main()  {
+	h1, err := getHash("test1.txt")
+	if err != nil{
+		return
+	}
+	h2, err := getHash("test2.txt")
+	if err != nil{
+		return
+	}
+	fmt.Println(h1, h2, h1 == h2)
 }
