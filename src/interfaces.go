d74e873a8238a993779db0d8167e0dda85b01264
mhope-2 <hopemichael720@gmail.com>
2021-07-04 22:19:59 +0000

    Update interfaces.go

--- src/interfaces.go
+++ src/interfaces.go
@@ -1,22 +1,44 @@
 package main
+
 import (
 	"fmt"
+	"math"
+
 )
 
-type Shape interface {
+type Shape interface{
 	area() float64
+} 
+
+type Circle struct {
+	x,y,radius float64
 }
 
+type Rectangle struct {
+	length, width float64
+}
 
-func totalArea(shapes ...Shape) float64 { 
-	var area float64 =0.0
-	for _, s := range shapes { 
-		area += s.area()
-	}
-	return area 
+// circle area
+func (c Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+// rectangle area
+func (r Rectangle) area() float64 {
+	return r.length * r.width
+}
+
+// 
+func getArea(s Shape) float64 {
+	return s.area()
 }
 
 func main(){
+	
+	rect1 := Rectangle{length: 12, width:4}
+	circle1 := Circle{x:0, y:0, radius: 7}
 
-	fmt.Println("")
+	fmt.Println(getArea(rect1))
+	fmt.Println(getArea(circle1))
+	
 }
